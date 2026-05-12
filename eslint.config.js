0482f1bed86080ae2b18eca3a94341619844ae55
Samuel Reichert <samuelreichertt@gmail.com>
2026-05-12 16:46:44 +0200

    chore: configure app tooling

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,20 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettierConfig = require('eslint-config-prettier');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
 
 module.exports = defineConfig([
   expoConfig,
+  prettierConfig,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+    },
+  },
   {
     ignores: ['dist/*'],
   },
